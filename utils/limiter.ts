9c9f10ad90575ebfba8ddb7499aa90e4e59f8075
xkiixr <kiie.sixnine@gmail.com>
2025-07-19 19:54:56 +0700

    deploy

--- utils/limiter.ts
+++ utils/limiter.ts
@@ -1,7 +1,8 @@
-import rateLimit from "express-rate-limit";
+import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
   windowMs: 30 * 1000,
   max: 10,
-  message: "Too many requests from this IP, please try again later.",
+
+  message: 'Too many requests from this IP, please try again later.',
 });
